fc50bc022a2b0ff54b62dcba82ab68e1f1041104
joshua.cho <joshua.cho@toss.im>
2023-06-14 15:23:34 +0900

    wip3

---
+++ Sources/Operator.swift
@@ -0,0 +1,12 @@
+//
+//  Operator.swift
+//  CoreStore
+//
+//  Created by 조수환 on 2023/06/14.
+//  Copyright © 2023 John Rommel Estropia. All rights reserved.
+//
+
+import Foundation
+
+infix operator .= : AssignmentPrecedence
+infix operator .== : ComparisonPrecedence
